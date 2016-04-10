3fbbd69e8253df72be2ebe77aed334bf36123eee
James Feigel <james.feigel@sq1.com>
2016-04-10 09:03:25 -0500

    Initial Taiga plugin support

---
+++ models/plugins/taiga.js
@@ -0,0 +1,24 @@
+"use strict";
+
+module.exports.process = (header, data) => {
+	// TODO: check for hash correctness?
+	// assume all checks have passed here
+	const returnObj = taigaProcessing["activity"](data);
+	returnObj.error = false;
+	returnObj.icon = "fa-certificate";
+	returnObj.avatar = "/assets/img/taiga-logo.svg";
+	returnObj.plugin = "taiga";
+	returnObj.user = data.data.owner.name;
+	// send it back
+	return returnObj;
+};
+
+const taigaProcessing = {
+	activity: (data) => {
+		return {
+			type: "activity",
+			title: `Project: ${data.data.project} - #${data.data.id} [${data.type}]`,
+			content: `${data.data.subject}<br>${data.data.description}`
+		};
+	}
+};
