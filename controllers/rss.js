240a47b40fe890cdf331c356b58807c09a22e925
James Feigel <james.feigel@sq1.com>
2016-06-24 15:56:49 -0500

    Fixed FeedParser and added TechCrunch

--- controllers/rss.js
+++ controllers/rss.js
@@ -7,70 +7,82 @@ const rss_options = config.site.rss_options;
 const Promise = require("bluebird");
 const FeedParser = require("feedparser");
 const request = require("request");
+const _ = require("lodash");
+const moment = require("moment");
+
 const socket = require("./sockets");
 
-const feedparser = new FeedParser();
-
 module.exports.init = function init() {
-  return new Promise(function promise(resolve, reject) {
-    const posts = [];
-    let meta;
+  return new Promise((resolveAll, rejectAll) => {
+    const rss_feed_promises = [];
 
-    const req = request({
-      url: rss[0].url,
-      headers: {
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36",
-        "accept": "text/html,application/xhtml+xml"
-      }
-    });
+    for (const rss_feed of rss) {
+      rss_feed_promises.push(new Promise((resolve, reject) => {
+        const posts = [];
+        let meta;
+        const feedparser = new FeedParser();
 
-    req.on("error", (err) => {
-      console.error("ERROR!", err);
-      reject(new Error("Bad request"));
-    });
-
-    req.on("response", (res) => {
-      if (res.statusCode !== 200) {
-        console.error("ERROR!", "Bad status code");
-        reject(new Error("Bad status code"));
-      }
-      console.log("req received 'response' event");
-      req.pipe(feedparser);
-    });
-
-    req.on("end", () => {
-      console.log("req received 'end' event");
-    });
-
-    feedparser.on("error", done);
-    feedparser.on("end", done);
-    feedparser.on("readable", function readable() {
-      let post;
-      meta = this.meta;
-      while (post = this.read()) {
-        posts.push({
-          error: false,
-          plugin: "rss",
-          type: "update",
-          title: post.title,
-          content: `${post.summary} <a href="${post.origlink || post.link}" target="_blank">Read More</a>`,
-          avatar: meta.image.url,
-          timestamp: post.pubdate
+        const req = request({
+          url: rss_feed.url
         });
-      }
-    });
 
-    function done(err) {
-      if (err) {
-        console.error("ERROR!", err);
-        reject(new Error("Error reading RSS feed"));
-      }
-      console.log("FeedParser received 'end' event");
-      resolve(posts);
+        req.on("error", (err) => {
+          console.error("ERROR!", err);
+          reject(new Error("Bad request"));
+        });
+
+        req.on("response", (res) => {
+          if (res.statusCode !== 200) {
+            console.error("ERROR!", "Bad status code");
+            reject(new Error("Bad status code"));
+          }
+
+          req.pipe(feedparser);
+        });
+
+        feedparser.on("error", done);
+        feedparser.on("end", done);
+        feedparser.on("readable", function readable() {
+          let post;
+          meta = this.meta;
+          while (post = this.read()) {
+            let content = post.summary;
+            if (rss_feed.read_more) {
+              content = `${content} <a href="${post.origlink || post.link}" target="_blank">Read More</a>`;
+            }
+            posts.push({
+              error: false,
+              plugin: "rss",
+              provider: rss_feed.provider,
+              type: "update",
+              title: post.title,
+              content: content,
+              avatar: meta.image.url,
+              timestamp: post.pubdate
+            });
+          }
+        });
+
+        function done(err) {
+          if (err) {
+            console.error("ERROR!", err);
+            reject(new Error("Error reading RSS feed"));
+          }
+
+          resolve(posts);
+        }
+      }));
     }
+
+    Promise.all(rss_feed_promises).then((all_feed_data) => {
+      const results = _.chain(all_feed_data)
+        .flatten()
+        .sortBy((feed_data) => {
+          return moment(feed_data.timestamp).unix() * -1;
+        })
+        .value();
+
+      resolveAll(results);
+    }, rejectAll);
   });
-};
-
-module.exports.process = function process() {
-
 };
